1f3d608217a05390a5a0bd98c992bd5b2d0ad555
Vlad.Zherko <vladikon19@tut.by>
2024-01-13 23:09:08 +0300

    little fixes

--- src/main/java/com/example/coursework/domain/ProductSearchDto.java
+++ src/main/java/com/example/coursework/domain/ProductSearchDto.java
@@ -1,13 +1,11 @@
 package com.example.coursework.domain;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
 @Setter
