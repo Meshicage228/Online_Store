f0a73374dfc021a5cbb28090cdde6a678f3e7c0c
Vlad.Zherko <vladikon19@tut.by>
2024-01-17 20:07:20 +0300

    init

---
+++ AdminBLService/src/main/java/com/example/adminblservice/AdminBlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.adminblservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminBlServiceApplication.class, args);
+    }
+
+}
