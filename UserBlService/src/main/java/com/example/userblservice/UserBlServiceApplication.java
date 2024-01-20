6a39d70d23f16589488029cdfbac6803b713d86d
Vlad.Zherko <vladikon19@tut.by>
2024-01-20 22:01:46 +0300

    init

---
+++ UserBlService/src/main/java/com/example/userblservice/UserBlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.userblservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBlServiceApplication.class, args);
+    }
+
+}
---
+++ UserBlService/src/test/java/com/example/userblservice/UserBlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userblservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
