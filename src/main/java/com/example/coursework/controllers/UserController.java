91dc50083dddae108ffcb14fe341d89bff0fbfba
Vlad.Zherko <vladikon19@tut.by>
2024-01-22 15:56:01 +0300

    deleted

--- src/main/java/com/example/coursework/controllers/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursework.controllers;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Configuration
-@RequestMapping("*/user")
-public class UserController {
-}
