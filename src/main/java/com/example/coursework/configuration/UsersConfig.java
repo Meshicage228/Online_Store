0aabe6b0c7540079b480791440d96d498430c0c5
Vlad.Zherko <vladikon19@tut.by>
2024-01-28 21:50:13 +0300

    deleted

--- src/main/java/com/example/coursework/configuration/UsersConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursework.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class UsersConfig {
-}
--- src/main/java/com/example/coursework/exceptions/IdNotFountException.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursework.exceptions;
-
-public class IdNotFountException extends RuntimeException{
-    private String message;
-
-    public IdNotFountException(String message) {
-        this.message = message;
-    }
-}
