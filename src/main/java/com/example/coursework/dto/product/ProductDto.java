84a1a4ee6042522f1c08e90822f77f51a07e968b
Vlad.Zherko <vladikon19@tut.by>
2024-02-26 16:54:37 +0300

    added validated

--- src/main/java/com/example/coursework/dto/product/ProductDto.java
+++ src/main/java/com/example/coursework/dto/product/ProductDto.java
@@ -4,6 +4,8 @@ import com.example.coursework.dto.user.CommentaryDto;
 import com.example.coursework.utils.annotations.CheckFileIsEmpty;
 import com.example.coursework.utils.annotations.CheckFileSize;
 import com.example.coursework.utils.annotations.CheckIsZero;
+import com.example.coursework.utils.markers.OnCreateMarker;
+import com.example.coursework.utils.markers.OnUpdateMarker;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,30 +23,30 @@ public class ProductDto {
 
     private Float bill;
 
-    @CheckFileIsEmpty
-    @NotNull
-    @CheckFileSize(maxSizeInMB = 5, message = "{app.file-size-message}")
+    @CheckFileIsEmpty(groups = OnCreateMarker.class)
+    @NotNull(groups = OnCreateMarker.class)
+    @CheckFileSize(maxSizeInMB = 5, message = "{app.file-size-message}", groups = OnCreateMarker.class)
     private List<MultipartFile> fileImage;
 
     private List<byte[]> imagesToThrow;
 
-    @NotEmpty(message = "Введите название продукта")
-    @Size(min = 1, max = 200, message = "Название в пределах от 1 до 200 символов")
+    @NotEmpty(message = "Введите название продукта", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Size(min = 1, max = 200, message = "Название в пределах от 1 до 200 символов", groups = {OnCreateMarker.class, OnUpdateMarker.class})
     private String title;
 
-    @NotEmpty(message = "Введите описание продукта")
-    @Size(min = 1, max = 200, message = "Описание должено быть в пределах 1 и 200 символов")
+    @NotEmpty(message = "Введите описание продукта", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Size(min = 1, max = 200, message = "Описание должено быть в пределах 1 и 200 символов", groups = {OnCreateMarker.class, OnUpdateMarker.class})
     private String description;
 
-    @NotNull(message = "Введите цену продукта")
-    @CheckIsZero(message = "Цена не может быть 0 byn")
-    @Min(value = 1, message = "Цена не может быть отрицательной")
-    @Max(value = 10000, message = "Цена не должна превышать 10000 byn")
+    @NotNull(message = "Введите цену продукта", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @CheckIsZero(message = "Цена не может быть 0 byn", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Min(value = 1, message = "Цена не может быть отрицательной", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Max(value = 10000, message = "Цена не должна превышать 10000 byn", groups = {OnCreateMarker.class, OnUpdateMarker.class})
     private Float price;
 
-    @NotNull(message = "Введите количество")
-    @Min(value = 1, message = "Минимально-допустимое значение : 1")
-    @Max(value = 5000, message = "Максимально-допустимое значение : 5000")
+    @NotNull(message = "Введите количество", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Min(value = 1, message = "Минимально-допустимое значение : 1", groups = {OnCreateMarker.class, OnUpdateMarker.class})
+    @Max(value = 5000, message = "Максимально-допустимое значение : 5000", groups = {OnCreateMarker.class, OnUpdateMarker.class})
     private Integer count;
 
     private List<ProductImageDto> images;
---
+++ src/main/java/com/example/coursework/utils/markers/OnUpdateMarker.java
@@ -0,0 +1,4 @@
+package com.example.coursework.utils.markers;
+
+public interface OnUpdateMarker {
+}
