d15123968cb2375dd0fc915cfaf86d08c6618535
Vlad.Zherko <vladikon19@tut.by>
2025-01-27 23:45:23 +0300

    refactored: remade code using stream api

--- MainModule/src/main/java/com/example/coursework/configuration/token/TokenGenerationFilter.java
+++ MainModule/src/main/java/com/example/coursework/configuration/token/TokenGenerationFilter.java
@@ -7,19 +7,17 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
-import static java.util.Objects.nonNull;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-
-@RequiredArgsConstructor
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class TokenGenerationFilter extends OncePerRequestFilter {
     private static final String TOKEN_NAME = "token";
     private static final String PASS_PARAM = "password";
@@ -28,20 +26,22 @@ public class TokenGenerationFilter extends OncePerRequestFilter {
     private final BCryptPasswordEncoder encoder;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         String password = request.getParameter(PASS_PARAM);
 
         String username = request.getParameter(AUTH_PARAM);
 
-        if (nonNull(username) && isNotBlank(username)) {
-            UserDetails userDetails = tokenService.loadUserByUsername(username);
-            if(nonNull(userDetails)) {
-                if (encoder.matches(password, userDetails.getPassword())) {
+        Optional.ofNullable(username)
+                .filter(StringUtils::isNotBlank)
+                .map(tokenService::loadUserByUsername)
+                .filter(userDetails -> encoder.matches(password, userDetails.getPassword()))
+                .ifPresent(userDetails -> {
                     String token = tokenService.createToken(userDetails);
                     response.addCookie(new Cookie(TOKEN_NAME, token));
-                }
-            }
-        }
+                });
+
         filterChain.doFilter(request, response);
     }
 }
--- MainModule/src/main/java/com/example/coursework/decoder/FeignErrorDecoder.java
+++ MainModule/src/main/java/com/example/coursework/decoder/FeignErrorDecoder.java
@@ -6,30 +6,61 @@ import com.example.coursework.utils.ErrorMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.apache.commons.lang3.StringUtils.startsWith;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+@Slf4j
+@RequiredArgsConstructor
 public class FeignErrorDecoder implements ErrorDecoder {
+    private final ObjectMapper mapper;
+
+    private final static Map<Predicate<Integer>, Function<ErrorMessage, Exception>> errorHandlers = new HashMap<>() {{
+        put(value -> value == NOT_FOUND.value(), errorMessage -> {
+            if (startsWith(errorMessage.getServiceName(), "product")) {
+                return new ProductNotFoundException(errorMessage.getMessage() != null ? errorMessage.getMessage() : "Not found");
+            } else if (startsWith(errorMessage.getServiceName(), "user")) {
+                return new UserNotFoundException(errorMessage.getMessage() != null ? errorMessage.getMessage() : "Not found");
+            }
+            return new RuntimeException("Resource not found");
+        });
+    }};
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        ErrorMessage message = null;
         try {
-            byte[] arr = response.body().asInputStream().readAllBytes();
-            var mapper = new ObjectMapper();
-            message = mapper.readValue(arr, ErrorMessage.class);
+            ErrorMessage errorMessage = parseErrorResponse(response);
+            return handleError(response.status(), errorMessage);
         } catch (IOException e) {
-            return new Exception(e.getMessage());
+            String readFailed = "Failed to read error message: " + e.getMessage();
+            log.error(readFailed);
+            return new RuntimeException(readFailed);
         }
-        if (response.status() > 400 && response.status() < 500) {
-            if (startsWith(message.getServiceName(), "product")) {
-                return new ProductNotFoundException(message.getMessage() != null ? message.getMessage() : "Not found");
-            } else if (startsWith(message.getServiceName(), "user")) {
-                return new UserNotFoundException(message.getMessage() != null ? message.getMessage() : "Not found");
-            }
-        }
-        return new RuntimeException();
+    }
+
+    private ErrorMessage parseErrorResponse(Response response) throws IOException {
+        byte[] arr = response.body().asInputStream().readAllBytes();
+        return mapper.readValue(arr, ErrorMessage.class);
+    }
+
+    private Exception handleError(int status, ErrorMessage errorMessage) {
+        return errorHandlers.entrySet().stream()
+                .filter(entry -> entry.getKey().test(status))
+                .map(entry -> entry.getValue().apply(errorMessage))
+                .findFirst()
+                .orElseThrow(() -> {
+                    String errorReason = Optional.ofNullable(errorMessage.getMessage()).orElse("Unknown error");
+                    log.error("No handler for status : {}; exception message : {}", status, errorReason);
+                    return new RuntimeException(errorReason);
+                });
     }
 }
